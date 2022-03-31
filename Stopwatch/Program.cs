85901230407ae8a6374f5ae29869635061a3c060
Mateusz Marszałek <marszalek.mateusz@gmail.com>
2022-03-31 12:38:29 +0200

    SimpleStopWatch

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newStopwatch = new Stopwatch();
+
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Write number for: ");
+                Console.WriteLine("1. Start");
+                Console.WriteLine("2. Stop");
+                Console.WriteLine("3. Duration");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("");
+
+                var validateInput = int.TryParse(Console.ReadLine(), out var input);
+
+
+                if (!validateInput)
+                {
+                    Console.Clear();
+                    Console.WriteLine("An invalid command was given");
+
+                }
+                else
+                {
+                    var exit = false;
+
+                    switch (input)
+                    {
+
+                        case 1:
+                            Console.Clear();
+                            newStopwatch.Start();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            newStopwatch.Stop();
+                            break;
+                        case 3:
+                            Console.Clear();
+                            newStopwatch.DisplayDuration();
+                            break;
+                        case 4:
+                            exit = true;
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("An invalid command was given. Please try again");
+                            break;
+                            
+                    }
+
+                    if (exit == true)
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        private DateTime DateStart { get; set; }
+
+        private DateTime DateStop { get; set; }
+
+        private bool Working { get; set; }
+
+
+        public Stopwatch()
+        {
+            Working = false;
+        }
+
+    public void Start()
+        {
+            if (!Working)
+            {
+                Working = true;
+                DateStart = DateTime.Now;
+               
+            }
+            else
+            {
+                Console.WriteLine("First stop the stopwatch");
+            }
+        }
+
+        public void Stop()
+        {
+            if (Working)
+            {
+                Working = false;
+                DateStop = DateTime.Now;
+            }
+            else
+            {
+                Console.WriteLine("First start the stopwatch");
+            }
+
+        }
+
+        public void DisplayDuration()
+        {
+            if (Working)
+            {
+                Console.WriteLine("First stop the stopwatch");
+            }
+            else if (DateStop == DateStart)
+            {
+                Console.WriteLine("First start the stopwatch");
+            }
+            else
+            {
+                Console.WriteLine("Duration: " + (DateStop - DateStart));
+            }
+        }
+
+    }
+}
